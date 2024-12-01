82c41b1c040157b3c6eed7e665b879f92dcd75f8
Ferenc Hechler <github@fh.anderemails.de>
2024-12-01 09:41:49 +0100

    feri day 01 part 2.

--- exchange/day01/feri/java/Y24Day01.java
+++ exchange/day01/feri/java/Y24Day01.java
@@ -44,6 +44,29 @@ public class Y24Day01 {
 	
 	
 	public static void mainPart2(String inputfile) throws FileNotFoundException {
+		
+		List<Integer> leftNumbers = new ArrayList<>();
+		List<Integer> rightNumbers = new ArrayList<>();
+		try (Scanner scanner = new Scanner(new File(inputfile))) {
+			while (scanner.hasNext()) {
+				String line = scanner.nextLine().trim();
+				if (line.isEmpty()) {
+					continue;
+				}
+				String[] numbers = line.split("   ");
+				int left = Integer.parseInt(numbers[0]);
+				int right = Integer.parseInt(numbers[1]);
+				leftNumbers.add(left);
+				rightNumbers.add(right);
+			}
+		}
+		long similarity = 0;
+		for (int i=0; i<leftNumbers.size(); i++) {
+			int left = leftNumbers.get(i);
+			long countRight = rightNumbers.stream().filter(right -> right == left).count();
+			similarity = similarity + left * countRight;
+		}
+		System.out.println("Similarity: "+similarity);
 	}
 
 	public static void main(String[] args) throws FileNotFoundException {
@@ -53,8 +76,8 @@ public class Y24Day01 {
 		System.out.println("---------------");
 		System.out.println();
 		System.out.println("--- PART II ---");
-		mainPart2("exchange/day01/feri/input-example.txt");
-//		mainPart2("exchange/day01/feri/input.txt");
+//		mainPart2("exchange/day01/feri/input-example.txt");
+		mainPart2("exchange/day01/feri/input.txt");
 		System.out.println("---------------");
 	}
 
